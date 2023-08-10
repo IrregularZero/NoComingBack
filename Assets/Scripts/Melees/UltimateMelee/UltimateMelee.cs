324d27e8a6b85d5e21ade582a57ce161ef0e16a3
IrregularZero <rocksoldier2005@gmail.com>
2023-08-10 17:53:56 +0200

    Added Wave projectile, Started implementing it

--- Assets/Scripts/Melees/UltimateMelee/UltimateMelee.cs
+++ Assets/Scripts/Melees/UltimateMelee/UltimateMelee.cs
@@ -22,6 +22,11 @@ public class UltimateMelee : MonoBehaviour
     private float shieldChangeStateTimer;
     private float previousDamageResist;
 
+    [SerializeField]
+    private GameObject WavePrefab;
+    [SerializeField]
+    private float WaveDamage;
+
     private Animator animator;
 
     #region Properties
