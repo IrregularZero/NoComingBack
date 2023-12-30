80fcc7f5fcc3d34294437c07220ed59703f6d690
IrregularZero <rocksoldier2005@gmail.com>
2023-12-30 16:11:50 +0100

    Implemented conf block to save slot buttons

--- Assets/Scripts/MenuScripts/Buttons/MainMenu/SaveSlot_btn_scr.cs
+++ Assets/Scripts/MenuScripts/Buttons/MainMenu/SaveSlot_btn_scr.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class SaveSlot_btn_scr : MainMenu_btn_scr
 {
+    private bool starting = false;
     [SerializeField]
     private int saveSlotIndex; // 1 - 4
 
+    [SerializeField]
+    private string message;
+    [SerializeField]
+    private string positive;
+    [SerializeField]
+    private string negative;
+
+    private void Update()
+    {
+        if (answerChanged)
+        {
+            answerChanged = false;
+            starting = answerFromConfBlock;
+        }
+        if (starting)
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().StartGame(saveSlotIndex);
+        }
+    }
+
     public override void ButtonPress()
     {
-        GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().StartGame(saveSlotIndex);
+        GameObject.FindGameObjectWithTag("ConfirmationBlock").GetComponent<ConfirmationBlock>().SetupAndShow(message, positive, negative, gameObject);
     }
 }
